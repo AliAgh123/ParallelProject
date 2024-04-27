709308f7ee7bd0908509ba68b9ade2e4e9f5965f
AliAgh123 <aliabdelgh@gmail.com>
2024-04-27 16:16:23 +0300

    added sudoku generator

--- DLX.cpp
+++ DLX.cpp
@@ -2,14 +2,23 @@
 #include <cmath>
 #include <string>
 #include <ctime>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
 
 using namespace std;
 
 #define MAX_K 1000
 
-#define SIZE 16
-//#define SIZE 9
+//#define SIZE 16
+#define SIZE 9
 //#define SIZE 4
+
+const int UNASSIGNED = 0;
+
+// Define the type for a single Sudoku puzzle (2D array)
+typedef vector<vector<int>> SudokuPuzzle;
 
 struct Node {
 
@@ -247,7 +256,7 @@ void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
 }
 
 //-------------------COVERS VALUES THAT ARE ALREADY PRESENT IN THE GRID-------------------------//
-void TransformListToCurrentGrid(int Puzzle[][SIZE]) {
+void TransformListToCurrentGrid(vector<vector<int>> Puzzle) {
     int index = 0;
     for(int i = 0 ; i<SIZE; i++ )
         for(int j = 0 ; j<SIZE; j++)
@@ -326,7 +335,7 @@ void PrintGrid(int Sudoku[][SIZE]){
 
 //--------------------------------------------------------------------------------//
 
-void SolveSudoku(int Sudoku[][SIZE]) {
+void SolveSudoku(vector<vector<int>> Sudoku) {
     timer = clock();
     BuildSparseMatrix(matrix);
     BuildLinkedList(matrix);
@@ -337,29 +346,72 @@ void SolveSudoku(int Sudoku[][SIZE]) {
     isSolved = false;
 }
 
+
+
+// Function to read Sudoku puzzles from file
+vector<SudokuPuzzle> readSudokuPuzzlesFromFile(const string& filename) {
+    ifstream inFile(filename);
+    vector<SudokuPuzzle> puzzles;
+    if (!inFile.is_open()) {
+        cerr << "Error: Unable to open file." << endl;
+        return puzzles;
+    }
+    string line;
+    while (getline(inFile, line)) {
+        int i = 0;
+        SudokuPuzzle puzzle(SIZE, vector<int>(SIZE, UNASSIGNED));
+        while(i < SIZE){
+            stringstream ss(line);
+            for (int j = 0; j < SIZE; ++j) {
+                ss >> puzzle[i][j];
+            }
+            getline(inFile, line);
+            i++;
+        }
+//        for(int i=0; i<SIZE; i++){
+//            for(int j=0; j<SIZE; j++){
+//                cout<<puzzle[i][j]<<" ";
+//            }cout<<endl;
+//        }
+//        cout<<"\n\n";
+        puzzles.push_back(puzzle);
+    }
+    inFile.close();
+    return puzzles;
+}
 int main(){
+
+    string filename = "sudoku_puzzles.txt";
+    vector<SudokuPuzzle> puzzles = readSudokuPuzzlesFromFile(filename);
+    if (puzzles.empty()) {
+        cout << "No Sudoku puzzles found in the file." << endl;
+        return 1;
+    }
+    cout << "Sudoku puzzles read from file: " << puzzles.size() << endl;
     //Sudoku Hard to Brute Force
-    int Puzzle[16][16] =		{ // 5 min board
-            {0, 15, 0, 1, 0, 2, 10, 14, 12, 0, 0, 0, 0, 0, 0, 0},
-            {0, 6, 3, 16, 12, 0, 8, 4, 14, 15, 1, 0, 2, 0, 0, 0},
-            {14, 0, 9, 7, 11, 3, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {4, 13, 2, 12, 0, 0, 0, 0, 6, 0, 0, 0, 0, 15, 0, 0},
-            {0, 0, 0, 0, 14, 1, 0, 0, 3, 5, 10, 0, 0, 8, 0, 12},
-            {3, 16, 0, 0, 2, 4, 0, 0, 0, 14, 7, 13, 0, 0, 5, 15},
-            {11, 0, 5, 0, 0, 0, 0, 0, 0, 9, 4, 0, 0, 6, 0, 0},
-            {0, 0, 0, 0, 13, 0, 16, 5, 15, 0, 0, 12, 0, 0, 0, 0},
-            {0, 0, 0, 0, 9, 0, 1, 12, 0, 8, 3, 10, 11, 0, 15, 0},
-            {2, 12, 0, 11, 0, 0, 14, 3, 5, 4, 0, 0, 0, 0, 9, 0},
-            {6, 3, 0, 4, 0, 0, 13, 0, 0, 11, 9, 1, 0, 12, 16, 2},
-            {0, 0, 10, 9, 0, 0, 0, 0, 0, 0, 12, 0, 8, 0, 6, 7},
-            {12, 8, 0, 0, 16, 0, 0, 10, 0, 13, 0, 0, 0, 5, 0, 0},
-            {5, 0, 0, 0, 3, 0, 4, 6, 0, 1, 15, 0, 0, 0, 0, 0},
-            {0, 9, 1, 6, 0, 14, 0, 11, 0, 0, 2, 0, 0, 0, 10, 8},
-            {0, 14, 0, 0, 0, 13, 9, 0, 4, 12, 11, 8, 0, 0, 2, 0}
-    };
+//    int Puzzle[16][16] =		{ // 5 min board
+//            {0, 15, 0, 1, 0, 2, 10, 14, 12, 0, 0, 0, 0, 0, 0, 0},
+//            {0, 6, 3, 16, 12, 0, 8, 4, 14, 15, 1, 0, 2, 0, 0, 0},
+//            {14, 0, 9, 7, 11, 3, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//            {4, 13, 2, 12, 0, 0, 0, 0, 6, 0, 0, 0, 0, 15, 0, 0},
+//            {0, 0, 0, 0, 14, 1, 0, 0, 3, 5, 10, 0, 0, 8, 0, 12},
+//            {3, 16, 0, 0, 2, 4, 0, 0, 0, 14, 7, 13, 0, 0, 5, 15},
+//            {11, 0, 5, 0, 0, 0, 0, 0, 0, 9, 4, 0, 0, 6, 0, 0},
+//            {0, 0, 0, 0, 13, 0, 16, 5, 15, 0, 0, 12, 0, 0, 0, 0},
+//            {0, 0, 0, 0, 9, 0, 1, 12, 0, 8, 3, 10, 11, 0, 15, 0},
+//            {2, 12, 0, 11, 0, 0, 14, 3, 5, 4, 0, 0, 0, 0, 9, 0},
+//            {6, 3, 0, 4, 0, 0, 13, 0, 0, 11, 9, 1, 0, 12, 16, 2},
+//            {0, 0, 10, 9, 0, 0, 0, 0, 0, 0, 12, 0, 8, 0, 6, 7},
+//            {12, 8, 0, 0, 16, 0, 0, 10, 0, 13, 0, 0, 0, 5, 0, 0},
+//            {5, 0, 0, 0, 3, 0, 4, 6, 0, 1, 15, 0, 0, 0, 0, 0},
+//            {0, 9, 1, 6, 0, 14, 0, 11, 0, 0, 2, 0, 0, 0, 10, 8},
+//            {0, 14, 0, 0, 0, 13, 9, 0, 4, 12, 11, 8, 0, 0, 2, 0}
+//    };
 
+//    for(int i=0; i<puzzles.size(); i++){
+//        SolveSudoku(puzzles[i]);
+//    }
 
-    SolveSudoku(Puzzle);
 
     cin.get();
     return 0;
---
+++ sudokuGen.cpp
@@ -0,0 +1,176 @@
+//#include <iostream>
+//#include <fstream>
+//#include <vector>
+//#include <cmath>
+//#include <ctime>
+//using namespace std;
+//
+//class Sudoku {
+//public:
+//    int** mat;
+//    int N;
+//    int SRN;
+//    int K;
+//
+//    Sudoku(int N, int K) {
+//        this->N = N;
+//        this->K = K;
+//        double SRNd = sqrt(N);
+//        SRN = (int)SRNd;
+//        mat = new int*[N];
+//        for (int i = 0; i < N; i++) {
+//            mat[i] = new int[N];
+//            memset(mat[i], 0, N * sizeof(int));
+//        }
+//    }
+//
+//    void fillValues() {
+//        fillDiagonal();
+//        fillRemaining(0, SRN);
+//        removeKDigits();
+//    }
+//
+//    void fillDiagonal() {
+//        for (int i = 0; i < N; i = i + SRN) {
+//            fillBox(i, i);
+//        }
+//    }
+//
+//    bool unUsedInBox(int rowStart, int colStart, int num) {
+//        for (int i = 0; i < SRN; i++) {
+//            for (int j = 0; j < SRN; j++) {
+//                if (mat[rowStart + i][colStart + j] == num) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+//
+//    void fillBox(int row, int col) {
+//        int num;
+//        for (int i = 0; i < SRN; i++) {
+//            for (int j = 0; j < SRN; j++) {
+//                do {
+//                    num = randomGenerator(N);
+//                } while (!unUsedInBox(row, col, num));
+//                mat[row + i][col + j] = num;
+//            }
+//        }
+//    }
+//
+//    int randomGenerator(int num) {
+//        return (int)floor((float)(rand() / double(RAND_MAX) * num + 1));
+//    }
+//
+//    bool CheckIfSafe(int i, int j, int num) {
+//        return (unUsedInRow(i, num) && unUsedInCol(j, num) && unUsedInBox(i - i % SRN, j - j % SRN, num));
+//    }
+//
+//    bool unUsedInRow(int i, int num) {
+//        for (int j = 0; j < N; j++) {
+//            if (mat[i][j] == num) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    bool unUsedInCol(int j, int num) {
+//        for (int i = 0; i < N; i++) {
+//            if (mat[i][j] == num) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    bool fillRemaining(int i, int j) {
+//        if (j >= N && i < N - 1) {
+//            i = i + 1;
+//            j = 0;
+//        }
+//        if (i >= N && j >= N) {
+//            return true;
+//        }
+//        if (i < SRN) {
+//            if (j < SRN) {
+//                j = SRN;
+//            }
+//        }
+//        else if (i < N - SRN) {
+//            if (j == (int)(i / SRN) * SRN) {
+//                j = j + SRN;
+//            }
+//        }
+//        else {
+//            if (j == N - SRN) {
+//                i = i + 1;
+//                j = 0;
+//                if (i >= N) {
+//                    return true;
+//                }
+//            }
+//        }
+//        for (int num = 1; num <= N; num++) {
+//            if (CheckIfSafe(i, j, num)) {
+//                mat[i][j] = num;
+//                if (fillRemaining(i, j + 1)) {
+//                    return true;
+//                }
+//                mat[i][j] = 0;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    void removeKDigits() {
+//        int count = K;
+//        while (count != 0) {
+//            int cellId = randomGenerator(N * N) - 1;
+//            int i = (cellId / N);
+//            int j = cellId % N;
+//            if (mat[i][j] != 0) {
+//                count--;
+//                mat[i][j] = 0;
+//            }
+//        }
+//    }
+//
+//    void printSudoku(ofstream& outFile) {
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                outFile << to_string(mat[i][j]) + " ";
+//            }
+//            outFile << endl;
+//        }
+//        outFile << endl;
+//    }
+//};
+//
+//void generateSudoku(int n, int k, const string& filename) {
+//    srand(time(0));
+//    ofstream outFile(filename);
+//    if (!outFile.is_open()) {
+//        cout << "Error: Unable to open file." << endl;
+//        return;
+//    }
+//    for (int i = 0; i < k; i++) {
+//        Sudoku sudoku(n, 20); // Assuming K = 20
+//        sudoku.fillValues();
+//        sudoku.printSudoku(outFile);
+//    }
+//    outFile.close();
+//    cout << "Sudoku puzzles generated and stored in " << filename << " file." << endl;
+//}
+//
+//int main() {
+//    int n, k;
+//    cout << "Enter the size of each puzzle (n): ";
+//    cin >> n;
+//    cout << "Enter the number of Sudoku puzzles (k): ";
+//    cin >> k;
+//    string filename = "sudoku_puzzles.txt";
+//    generateSudoku(n, k, filename);
+//    return 0;
+//}
